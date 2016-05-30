65356ac972ecc21171bbe26f85c7667fda934b2e
Luca Weiss <WEI16416@spengergasse.at>
2016-05-30 09:31:51 +0200

    Add project files.

---
+++ VIennaRuns/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VIennaRuns.Startup))]
+namespace VIennaRuns
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
